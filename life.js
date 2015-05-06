51421af646329a6b9a31ae07d6b63b93cdb6e6c7
peel3r <p@weljakubowski.com>
2015-05-06 12:02:10 +0100

    add the view object

--- life.js
+++ life.js
@@ -64,7 +64,7 @@
 })();
 
 
-
+/* initial debugging helper
 var game = new Life([
     [0,0,0,0,0],
     [0,0,1,0,0],
@@ -73,8 +73,155 @@ var game = new Life([
     [0,0,0,0,0]
 ]);
 
-console.log(game +  '');
+console.log(game  + ' ');
 game.next();
-console.log(game  + '');
+console.log(game  + ' ');
 game.next();
-console.log(game  + '');
+console.log(game  + ' ');
+*/
+
+(function() {
+
+    var _ = self.Life = function(seed) {
+        this.seed = seed;
+        this.height = seed.length;
+        this.width = seed[0].length;
+
+        this.prevBoard = [];
+        this.board = cloneArray(seed);
+    };
+
+
+    _.prototype = {
+        createGrid: function () {
+            var me = this;
+
+            var fragment = document.createDocumentFragment();
+            this.grid.innerHTML = '';
+            this.checkboxes = [];
+
+            for (var y=0; y<this.size; y++) {
+                var row = document.createElement('tr');
+                this.checkboxes[y] = [];
+
+                for (var x=0; x<this.size; x++) {
+                    var cell = document.createElement('td');
+                    var checkbox = document.createElement('input');
+                    checkbox.type = 'checkbox';
+                    this.checkboxes[y][x] = checkbox;
+                    checkbox.coords = [y, x];
+
+                    cell.appendChild(checkbox);
+                    row.appendChild(cell);
+                }
+
+                fragment.appendChild(row);
+            }
+
+            this.grid.addEventListener('change', function(evt) {
+                if (evt.target.nodeName.toLowerCase() == 'input') {
+                    me.started = false;
+                }
+            });
+
+            this.grid.addEventListener('keyup', function(evt) {
+                var checkbox = evt.target;
+
+                if (checkbox.nodeName.toLowerCase() == 'input') {
+                    var coords = checkbox.coords;
+                    var y = coords[0];
+                    var x = coords[1];
+
+                    switch (evt.keyCode) {
+                        case 37: // left
+                            if (x > 0) {
+                                me.checkboxes[y][x-1].focus();
+                            }
+                            break;
+                        case 38: // up
+                            if (y > 0) {
+                                me.checkboxes[y-1][x].focus();
+                            }
+                            break;
+                        case 39: // right
+                            if (x < me.size - 1) {
+                                me.checkboxes[y][x+1].focus();
+                            }
+                            break;
+                        case 40: // bottom
+                            if (y < me.size - 1) {
+                                me.checkboxes[y+1][x].focus();
+                            }
+                            break;
+                    }
+                }
+            });
+
+            this.grid.appendChild(fragment);
+        },
+
+        get boardArray() {
+            return this.checkboxes.map(function (row) {
+                return row.map(function (checkbox) {
+                    return +checkbox.checked;
+                });
+            });
+        },
+
+        play: function () {
+            this.game = new Life(this.boardArray);
+            this.started = true;
+        },
+
+        next: function () {
+            var me = this;
+
+            if (!this.started || this.game) {
+                this.play();
+            }
+
+            this.game.next();
+
+            var board = this.game.board;
+
+            for (var y=0; y<this.size; y++) {
+                for (var x=0; x<this.size; x++) {
+                    this.checkboxes[y][x].checked = !!board[y][x];
+                }
+            }
+
+            if (this.autoplay) {
+                this.timer = setTimeout(function () {
+                    me.next();
+                }, 1000);
+            }
+        }
+    };
+
+    })();
+
+var lifeView = new LifeView(document.getElementById('grid'), 12);
+
+(function() {
+
+    var buttons = {
+        next: $('button.next')
+    };
+
+    buttons.next.addEventListener('click', function() {
+        lifeView.next();
+    });
+
+    $('#autoplay').addEventListener('change', function() {
+        buttons.next.disabled = this.checked;
+
+        if (this.checked) {
+            lifeView.autoplay = this.checked;
+            lifeView.next();
+        }
+        else {
+            clearTimeout(lifeView.timer);
+        }
+    });
+
+})();
